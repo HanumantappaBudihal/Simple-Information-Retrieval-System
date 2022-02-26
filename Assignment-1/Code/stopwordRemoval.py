06aa5f7d83855c922f90b65946f0ab87d3a19ab8
Hanumantappa Budihal <hanumanth.k.budihal@gmail.com>
2022-02-26 07:36:50 +0530

    Added the unit test case for Stop word removal

--- Assignment-1/Code/stopwordRemoval.py
+++ Assignment-1/Code/stopwordRemoval.py
@@ -2,36 +2,49 @@ from util import *
 import nltk
 nltk.download("stopwords")
 
-# Add your import statements here
-# Varun Gumma
-
-
 class StopwordRemoval():
 
-	def fromList(self, text):
-		"""
-		Sentence Segmentation using the Punkt Tokenizer
+    def fromList(self, text):
+        """
+        Sentence Segmentation using the Punkt Tokenizer
 
-		Parameters
-		----------
-		arg1 : list
-			A list of lists where each sub-list is a sequence of tokens
-			representing a sentence
+        Parameters
+        ----------
+        arg1 : list
+                A list of lists where each sub-list is a sequence of tokens
+                representing a sentence
 
-		Returns
-		-------
-		list
-			A list of lists where each sub-list is a sequence of tokens
-			representing a sentence with stopwords removed
-		"""
+        Returns
+        -------
+        list
+                A list of lists where each sub-list is a sequence of tokens
+                representing a sentence with stopwords removed
+        """
 
-		#Fill in code here
-		allStopwords = nltk.corpus.stopwords.words("english")
-		stopwordRemovedText = [[t for t in tokens if not t in allStopwords] for tokens in text]
+        # Fill in code here
+        allStopwords = nltk.corpus.stopwords.words("english")
+        stopwordRemovedText = [
+            [t for t in tokens if not t in allStopwords] for tokens in text]
 
-		return stopwordRemovedText
+        return stopwordRemovedText
 
+################################## Unit Testig #############################################
 
+import unittest
 
+class StopwordRemovalTestMethods(unittest.TestCase):
 
-	
+    def test_fromList(self):
+        # Arrange
+        text = ["He determined to drop his litigation with the monastry, and relinguish his claims to the wood-cuting and fishery rihgts at once.",
+		        "He was the more ready to do this becuase the rights had become much less valuable, and he had indeed the vaguest idea where the wood and river in question were."]
+
+        excepted_result = ["He determined drop litigation monastry, relinguish claims wood-cuting fishery rihgts.",
+						   "He ready becuase rights become much less valuable, indeed vaguest idea wood river question."]
+        # Act
+        actual_result = StopwordRemoval().fromList(text)
+        # Assestion
+        self.assertEqual(excepted_result, actual_result)
+
+if __name__ == '__main__':
+    unittest.main()
